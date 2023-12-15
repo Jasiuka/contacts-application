a25ef40dbb7ed75ed418f154ee38840effc1272f
studentas <lukas.jasiukaitis@teltonika.lt>
2023-12-15 08:11:44 +0200

    baseDropdown component

---
+++ src/components/Base/BaseDropdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="dropdown">
+        <div class="dropdown-display">
+            <slot name="display"></slot>
+        </div>
+        <ul class="dropdown-list">
+            <slot name="list-items"></slot>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.dropdown {
+    position: relative;
+    padding-bottom: var(--pd-smallest);
+}
+
+.dropdown:hover .dropdown-list {
+    opacity: 1;
+    visibility: visible;
+    pointer-events: all;
+}
+
+.dropdown-display {
+    font-size: inherit;
+    cursor: pointer;
+}
+.dropdown-list {
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: none;
+    position: absolute;
+    width: max-content;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    left: -20%;
+    top: 100%;
+    background-color: white;
+    border-radius: var(--border-radius);
+}
+</style>
--- src/components/NavigationDropdown.vue
+++ src/components/NavigationDropdown.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="dropdown">
-        <p class="dropdown-button">
+    <base-dropdown>
+        <template #display>
             {{ dropdownName }}
-        </p>
-        <ul class="dropdown-list">
+        </template>
+        <template #list-items>
             <li v-for="{ name, to } in links" :key="name">
                 <router-link class="dropdown-link nav-link" :to="to">{{
                     name
                 }}</router-link>
             </li>
-        </ul>
-    </div>
+        </template>
+    </base-dropdown>
 </template>
 
 <script>
+import BaseDropdown from "./Base/BaseDropdown.vue";
 export default {
+    components: {
+        BaseDropdown,
+    },
     props: {
         dropdownName: {
             type: String,
@@ -25,50 +29,10 @@ export default {
             required: true,
         },
     },
-    data() {
-        return {
-            dropdownVisible: false,
-        };
-    },
-    methods: {
-        toggleDropdown() {
-            this.dropdownVisible = !this.dropdownVisible;
-        },
-    },
 };
 </script>
 
 <style scoped>
-.dropdown {
-    position: relative;
-    padding-bottom: var(--pd-smallest);
-}
-
-.dropdown:hover .dropdown-list {
-    opacity: 1;
-    visibility: visible;
-    pointer-events: all;
-}
-
-.dropdown-button {
-    font-size: inherit;
-    cursor: pointer;
-}
-.dropdown-list {
-    opacity: 0;
-    visibility: hidden;
-    pointer-events: none;
-    position: absolute;
-    width: max-content;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    left: -20%;
-    top: 100%;
-    background-color: white;
-    border-radius: var(--border-radius);
-}
-
 .dropdown-list li {
     font-size: var(--fs-small);
     min-width: 100%;
