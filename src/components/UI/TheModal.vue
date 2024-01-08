6b4c7cb75e80332ff51c931b883ce2fdfc2b6d52
studentas <lukas.jasiukaitis@teltonika.lt>
2024-01-08 11:34:18 +0200

    fixed duplicate rendering bug

--- src/components/UI/TheModal.vue
+++ src/components/UI/TheModal.vue
@@ -3,7 +3,6 @@
         <div v-if="getModalVisible" class="modal-background"></div>
         <transition name="modal">
             <dialog class="dialog" v-if="getModalVisible" open>
-                <slot name="modal-component"></slot>
                 <button
                     v-if="!closeNotRequired"
                     @click="closeModal"
