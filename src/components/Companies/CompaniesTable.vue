4e090d3080496ce6925c4a30fa4f16f0a4e1d4b7
Jasiuka <luksjasiukaitis@gmail.com>
2024-01-15 16:57:14 +0200

    create account form and logic

--- src/components/Companies/CompaniesTable.vue
+++ src/components/Companies/CompaniesTable.vue
@@ -1,33 +1,33 @@
 <template>
-    <base-table data-table="simple">
-        <template #table-head>
-            <th>Įmonės pavadinimas</th>
-            <th v-if="actionsVisible">Veiksmai</th>
-        </template>
-        <template #table-body>
-            <tr :key="company.id" v-for="company in companies">
-                <td>{{ company.name }}</td>
-                <td class="row-actions" v-if="actionsVisible">
-                    <button
-                        v-if="getPermissions.edit_companies"
-                        @click="openModal(formTypes.EDIT_COMPANY, company)"
-                        title="Koreguoti"
-                        class="companies-edit action-edit action-btn"
-                    >
-                        Redaguoti
-                    </button>
-                    <button
-                        v-if="getPermissions.delete_companies"
-                        title="Ištrinti"
-                        class="companies-delete action-delete action-btn"
-                        @click="openModal(formTypes.DELETE_COMPANY, company)"
-                    >
-                        Ištrinti
-                    </button>
-                </td>
-            </tr>
-        </template>
-    </base-table>
+  <base-table data-table="simple">
+    <template #table-head>
+      <th>Įmonės pavadinimas</th>
+      <th v-if="actionsVisible">Veiksmai</th>
+    </template>
+    <template #table-body>
+      <tr :key="company.id" v-for="company in companies">
+        <td>{{ company.name }}</td>
+        <td class="row-actions" v-if="actionsVisible">
+          <button
+            v-if="getPermissions.edit_companies"
+            @click="openModal(formTypes.EDIT_COMPANY, company)"
+            title="Koreguoti"
+            class="companies-edit action-edit action-btn"
+          >
+            Redaguoti
+          </button>
+          <button
+            v-if="getPermissions.delete_companies"
+            title="Ištrinti"
+            class="companies-delete action-delete action-btn"
+            @click="openModal(formTypes.DELETE_COMPANY, company)"
+          >
+            Ištrinti
+          </button>
+        </td>
+      </tr>
+    </template>
+  </base-table>
 </template>
 
 <script>
@@ -35,36 +35,36 @@ import BaseTable from "../Base/BaseTable.vue";
 import { formTypes } from "../Forms/formTypes";
 import { mapMutations, mapGetters } from "vuex";
 export default {
-    name: "CompaniesTable",
-    components: {
-        BaseTable,
+  name: "CompaniesTable",
+  components: {
+    BaseTable,
+  },
+  data() {
+    return {
+      formTypes: formTypes,
+    };
+  },
+  props: {
+    companies: {
+      type: Array,
+      required: true,
     },
-    data() {
-        return {
-            formTypes: formTypes,
-        };
+  },
+  computed: {
+    ...mapGetters(["getPermissions"]),
+    actionsVisible() {
+      return (
+        this.getPermissions.edit_companies &&
+        this.getPermissions.delete_companies
+      );
     },
-    props: {
-        companies: {
-            type: Array,
-            required: true,
-        },
+  },
+  methods: {
+    ...mapMutations(["OPEN_MODAL", "SET_COMPANY_TO_MODIFY"]),
+    openModal(formType, company) {
+      this.SET_COMPANY_TO_MODIFY(company);
+      this.OPEN_MODAL(formType);
     },
-    computed: {
-        ...mapGetters(["getPermissions"]),
-        actionsVisible() {
-            return (
-                this.getPermissions.edit_companies &&
-                this.getPermissions.delete_companies
-            );
-        },
-    },
-    methods: {
-        ...mapMutations(["OPEN_MODAL", "SET_COMPANY_TO_MODIFY"]),
-        openModal(formType, company) {
-            this.SET_COMPANY_TO_MODIFY(company);
-            this.OPEN_MODAL(formType);
-        },
-    },
+  },
 };
 </script>
--- src/components/CustomCheckboxSelect.vue
+++ src/components/CustomCheckboxSelect.vue
@@ -1,61 +1,60 @@
 <template>
-    <div class="custom-checkbox-select">
-        <div class="select" v-for="option in options" :key="option.name">
-            <input
-                type="checkbox"
-                :id="option.name"
-                :name="option.name"
-                @change="check($event, option.name)"
-            />
-            <label :for="option.name">{{ option.label }}</label>
-        </div>
+  <div class="custom-checkbox-select">
+    <div class="select" v-for="option in options" :key="option.name">
+      <input
+        type="checkbox"
+        :id="option.name"
+        :name="option.name"
+        @change="check($event, option.name)"
+      />
+      <label :for="option.name">{{ option.label }}</label>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "CustomCheckboxSelect",
-    props: {
-        options: {
-            type: Array,
-            required: true,
-        },
+  name: "CustomCheckboxSelect",
+  props: {
+    options: {
+      type: Array,
+      required: true,
     },
-    data() {
-        return {
-            checks: {},
-        };
+  },
+  data() {
+    return {
+      checks: {},
+    };
+  },
+  methods: {
+    check(event, checkName) {
+      const value = event.target.checked;
+      this.checks[checkName] = value;
+      this.sendChecks();
     },
-    methods: {
-        check(event, checkName) {
-            const value = event.target.checked;
-            this.checks[checkName] = value;
-            console.log(this.checks);
-            // this.sendChecks();
-        },
-        sendChecks() {
-            this.$emit("send-checks", this.checks);
-        },
+    sendChecks() {
+      this.$emit("send-checks", this.checks);
     },
+  },
 };
 </script>
 
 <style scoped>
 .custom-checkbox-select {
-    display: flex;
-    flex-direction: column;
-    align-items: baseline;
-    gap: var(--gap-smallest);
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  gap: var(--gap-smallest);
 }
 .select {
-    display: flex;
-    align-items: center;
-    gap: var(--gap-small);
-    font-size: var(--fs-smallest);
+  display: flex;
+  align-items: center;
+  gap: var(--gap-small);
+  font-size: var(--fs-smallest);
 }
 
 .select input {
-    height: 1.5rem;
-    width: 1.5rem;
+  height: 1.5rem;
+  width: 1.5rem;
 }
 </style>
