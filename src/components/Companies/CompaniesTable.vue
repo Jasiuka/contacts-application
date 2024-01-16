265b47d43d2446ac71f451a46ade3eecab973004
studentas <lukas.jasiukaitis@teltonika.lt>
2024-01-16 13:24:32 +0200

    cherry picked

--- src/components/Companies/CompaniesTable.vue
+++ src/components/Companies/CompaniesTable.vue
@@ -1,33 +1,33 @@
 <template>
-  <base-table data-table="simple">
-    <template #table-head>
-      <th>Įmonės pavadinimas</th>
-      <th v-if="actionsVisible">Veiksmai</th>
-    </template>
-    <template #table-body>
-      <tr :key="company.id" v-for="company in companies">
-        <td>{{ company.name }}</td>
-        <td class="row-actions" v-if="actionsVisible">
-          <button
-            v-if="getPermissions.edit_companies"
-            @click="openModal(formTypes.EDIT_COMPANY, company)"
-            title="Koreguoti"
-            class="companies-edit action-edit action-btn"
-          >
-            Redaguoti
-          </button>
-          <button
-            v-if="getPermissions.delete_companies"
-            title="Ištrinti"
-            class="companies-delete action-delete action-btn"
-            @click="openModal(formTypes.DELETE_COMPANY, company)"
-          >
-            Ištrinti
-          </button>
-        </td>
-      </tr>
-    </template>
-  </base-table>
+    <base-table data-table="simple">
+        <template #table-head>
+            <th>Įmonės pavadinimas</th>
+            <th v-if="actionsVisible">Veiksmai</th>
+        </template>
+        <template #table-body>
+            <tr :key="company.id" v-for="company in companies">
+                <td>{{ company.name }}</td>
+                <td class="row-actions" v-if="actionsVisible">
+                    <button
+                        v-if="getPermissions.edit_companies"
+                        @click="openModal(formTypes.EDIT_COMPANY, company)"
+                        title="Koreguoti"
+                        class="companies-edit action-edit action-btn"
+                    >
+                        Redaguoti
+                    </button>
+                    <button
+                        v-if="getPermissions.delete_companies"
+                        title="Ištrinti"
+                        class="companies-delete action-delete action-btn"
+                        @click="openModal(formTypes.DELETE_COMPANY, company)"
+                    >
+                        Ištrinti
+                    </button>
+                </td>
+            </tr>
+        </template>
+    </base-table>
 </template>
 
 <script>
@@ -35,36 +35,36 @@ import BaseTable from "../Base/BaseTable.vue";
 import { formTypes } from "../Forms/formTypes";
 import { mapMutations, mapGetters } from "vuex";
 export default {
-  name: "CompaniesTable",
-  components: {
-    BaseTable,
-  },
-  data() {
-    return {
-      formTypes: formTypes,
-    };
-  },
-  props: {
-    companies: {
-      type: Array,
-      required: true,
+    name: "CompaniesTable",
+    components: {
+        BaseTable,
     },
-  },
-  computed: {
-    ...mapGetters(["getPermissions"]),
-    actionsVisible() {
-      return (
-        this.getPermissions.edit_companies &&
-        this.getPermissions.delete_companies
-      );
+    data() {
+        return {
+            formTypes: formTypes,
+        };
     },
-  },
-  methods: {
-    ...mapMutations(["OPEN_MODAL", "SET_COMPANY_TO_MODIFY"]),
-    openModal(formType, company) {
-      this.SET_COMPANY_TO_MODIFY(company);
-      this.OPEN_MODAL(formType);
+    props: {
+        companies: {
+            type: Array,
+            required: true,
+        },
     },
-  },
+    computed: {
+        ...mapGetters(["getPermissions"]),
+        actionsVisible() {
+            return (
+                this.getPermissions.edit_companies ||
+                this.getPermissions.delete_companies
+            );
+        },
+    },
+    methods: {
+        ...mapMutations(["OPEN_MODAL", "SET_COMPANY_TO_MODIFY"]),
+        openModal(formType, company) {
+            this.SET_COMPANY_TO_MODIFY(company);
+            this.OPEN_MODAL(formType);
+        },
+    },
 };
 </script>
