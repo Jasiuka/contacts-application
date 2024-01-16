f2408935dd6ba6d5c8cf83ba9f519bf3a43aab35
studentas <lukas.jasiukaitis@teltonika.lt>
2024-01-16 16:54:57 +0200

    auth token removed

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,5 @@ export default defineConfig({
     },
     define: {
         BASE_API_URL: JSON.stringify("http://127.0.0.1:8090/api/collections/"),
-        AUTH_TOKEN: JSON.stringify(
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJfcGJfdXNlcnNfYXV0aF8iLCJleHAiOjE3MDY2MDE1NTQsImlkIjoidWx6YWlxa2U4eDB4ZGkxIiwidHlwZSI6ImF1dGhSZWNvcmQifQ.UoFSH6L_ibksNTV3SvXb4gAkSq7uXyHgSosJitMf6cg"
-        ),
     },
 });
